84e899f8783ec2d6a226b9475d99abeadb50dc6d
gabrielhilva <gabrielhsilva863@gmaii.com>
2018-04-12 12:01:04 -0300

    exercicio js

---
+++ script.js
@@ -0,0 +1,22 @@
+let contador = 0;
+let limite = prompt("digite aqui seu numero");
+
+while(contador <= limite){
+    console.log(contador);
+    contador ++;
+}
+
+
+
+serquilha
+
+let tamanho=50;
+
+let contador=1;
+let escadinha="#";
+
+while(contador <= tamanho){
+    console.log(escadinha);
+    contador++;
+    escadinha=escadinha+ "#"
+}
